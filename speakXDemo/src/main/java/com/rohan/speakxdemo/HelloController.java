025b7138eb2c4883746cbe59c783abc8f6a9199c
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2025-01-19 03:22:17 +0530

    Initial Commit

---
+++ speakXDemo/src/main/java/com/rohan/speakxdemo/HelloController.java
@@ -0,0 +1,26 @@
+package com.rohan.speakxdemo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/")
+public class HelloController {
+
+
+    @GetMapping
+    public Map<String, String> home(){
+        Map<String, String> response = new HashMap<String, String>();
+        response.put("message", "Hello World");
+        response.put("status", "200");
+        response.put("timestamp", String.valueOf(System.currentTimeMillis()));
+        response.put("version", "1.0.0");
+        response.put("user", "rohan");
+        response.put("env", "docker");
+        return response;
+    }
+}
---
+++ speakXDemo/src/test/java/com/rohan/speakxdemo/SpeakXDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rohan.speakxdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpeakXDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
