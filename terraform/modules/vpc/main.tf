7d81416641e52866dcc2157968440eb59b5b2629
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2025-01-24 22:02:20 +0530

    Added Some Configuratiosn releted to SSL/TLS

--- terraform/modules/vpc/main.tf
+++ terraform/modules/vpc/main.tf
@@ -15,6 +15,8 @@ resource "aws_subnet" "app_subnet" {
   tags = {
     Name = "app-subnet"
   }
+
+  depends_on = [ aws_vpc.app_vpc ]
 }
 
 output "vpc_id" {
@@ -23,4 +25,31 @@ output "vpc_id" {
 
 output "subnet_id" {
   value = aws_subnet.app_subnet.id
+}
+
+
+resource "aws_internet_gateway" "app_igw" {
+  vpc_id = aws_vpc.app_vpc.id
+  tags = {
+    Name = "app-igw"
+  }
+}
+
+# Associate the route table with the subnet
+resource "aws_route_table" "app_route_table" {
+  vpc_id = aws_vpc.app_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.app_igw.id
+  }
+
+  tags = {
+    Name = "app-route-table"
+  }
+}
+
+resource "aws_route_table_association" "app_route_assoc" {
+  subnet_id      = aws_subnet.app_subnet.id
+  route_table_id = aws_route_table.app_route_table.id
 }
