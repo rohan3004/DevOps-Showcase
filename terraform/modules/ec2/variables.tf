ce9559a1d6b381dea274da36d8696c562838b397
Rohan Chakravarty <rohan.chakravarty02@gmail.com>
2025-01-25 02:04:57 +0530

    Nginx Configuration

--- terraform/modules/ec2/variables.tf
+++ terraform/modules/ec2/variables.tf
@@ -23,14 +23,39 @@ variable "user_data" {
   type        = string
   default     = <<-EOF
               #!/bin/bash
+              # Update and install dependencies
               sudo apt update
               sudo apt install -y nginx
               sudo systemctl enable nginx
               sudo systemctl start nginx
               sudo apt install -y certbot python3-certbot-nginx
+
+              # Nginx configuration for reverse proxy
+              echo '
+              server {
+                  listen 80;
+                  server_name devops.rohandev.online;
+
+                  location / {
+                      proxy_pass http://localhost:8080;
+                      proxy_http_version 1.1;
+                      proxy_set_header Upgrade $http_upgrade;
+                      proxy_set_header Connection 'upgrade';
+                      proxy_set_header Host $host;
+                      proxy_cache_bypass $http_upgrade;
+                      proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+                      proxy_set_header X-Forwarded-Proto $scheme;
+                  }
+              }' | sudo tee /etc/nginx/sites-available/default
+
+              # Obtain SSL certificate using Certbot
               sudo certbot --nginx --non-interactive --agree-tos --email rohan.chakravarty02@gmail.com -d devops.rohandev.online
+
+              # Reload Nginx to apply changes
+              sudo systemctl reload nginx
               EOF
 }
+
 
 variable "EC2_HOST" {
   type = string
